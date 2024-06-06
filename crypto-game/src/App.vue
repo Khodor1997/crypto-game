496c8c813e65c2ee1ae254f3959ae45d3241f7fc
DESKTOP-87IG1V9\Pavel <pasha_khodor@mail.ru>
2024-06-06 23:20:42 +0300

    add header

--- crypto-game/src/App.vue
+++ crypto-game/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader/>
+  <div class="main"></div>
 </template>
 
 <style scoped>
@@ -26,5 +20,10 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.main {
+  background: #111927;
+  width: 100%;
+  height: calc(100vh - 65px);
 }
 </style>
