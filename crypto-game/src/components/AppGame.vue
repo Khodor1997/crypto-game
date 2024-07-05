4c5b37409fa9c9de00b856e5055d463f7afd0725
DESKTOP-87IG1V9\Pavel <pasha_khodor@mail.ru>
2024-07-05 16:00:32 +0300

    added chart

--- crypto-game/src/components/AppGame.vue
+++ crypto-game/src/components/AppGame.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="game">
         <div class="eth-price">${{ ethPrice }}</div>
+        <div id="chart">
+            <VueApexCharts type="line" height="400" ref="chart" :options="chartOptions" :series="series"></VueApexCharts>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted  } from 'vue';
 import { getEthPrice } from '../api/getEthPrice';
+import VueApexCharts from "vue3-apexcharts";
 
-const ethPrice = ref<string | undefined>(undefined)
+const ethPrice = ref<number>(0)
 
 const getPrice = async () => {
     try {
         const price = await getEthPrice()
-        ethPrice.value = price.price
-        console.log( price, 'ffffffff');
+        ethPrice.value = Number(price.price)
         
     } catch (err) {
         console.log(err)
@@ -28,6 +31,131 @@ const getPrice = async () => {
 setInterval(() => {
     getPrice()
 }, 500);
+
+// const series = ref([{data: []}]);
+// let XAXISRANGE = 10000
+const data = ref(getInitialData());
+// const lastDate = ref(new Date().getTime());
+
+const series = ref([{ data: getInitialData() }]);
+const XAXISRANGE = 10000;
+const lastDate = ref(new Date().getTime());
+
+const chartOptions = {
+  chart: {
+    id: 'realtime',
+    height: 350,
+    type: 'line',
+    animations: {
+      enabled: true,
+      easing: 'linear',
+      dynamicAnimation: {
+        speed: 1000
+      }
+    },
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Dynamic Updating Chart',
+    align: 'left'
+  },
+  markers: {
+    size: 0
+  },
+  xaxis: {
+    type: 'datetime',
+    range: XAXISRANGE,
+    min: lastDate.value - 123100
+  },
+  yaxis: {
+    // show: true,
+    //   showAlways: false,
+    //   showForNullSeries: true,
+    //   seriesName: undefined,
+    //   opposite: false,
+    //   reversed: false,
+    //   logarithmic: true,
+    // //   logBase: 10,
+    //   tickAmount: undefined,
+      min: function() {
+        let min = 3678
+        if (ethPrice.value < min) {
+            min = ethPrice.value - 1
+        }
+        return min
+      },
+      max: function() {
+        let max = 3678
+        if (ethPrice.value > max) {
+            max = ethPrice.value + 1
+        }
+        return max
+      },
+    //   stepSize: undefined,
+    //   forceNiceScale: false,
+    //   floating: false,
+    //   decimalsInFloat: undefined,
+  },
+  legend: {
+    show: false
+  }
+};
+
+
+const updateSeries = () => {
+  getNewSeries(lastDate.value, { min: 1500, max: 4500 });
+  series.value = [{ data: data.value }];
+};
+
+const resetDataInterval = () => {
+  resetData();
+  series.value = [{ data: data.value }];
+};
+
+function resetData() {
+  data.value = getInitialData();
+}
+
+function getNewSeries(baseval, yrange) {
+  const newDate = baseval + 200;
+  lastDate.value = newDate;
+    
+  const newData = {
+    x: newDate,
+    y: ethPrice.value
+  };
+    
+  data.value.push(newData);
+
+  if (data.value.length > 1000) {
+    data.value.shift();
+  }
+}
+
+function getInitialData() {
+  const now = new Date().getTime();
+  return Array.from({ length: 10 }, (v, i) => ({
+    x: now - i * 1000,
+    y: Math.floor(Math.random() * 80) + 10
+  })).reverse();
+}
+
+onMounted(() => {
+  setInterval(updateSeries, 1000);
+//   setInterval(resetDataInterval, 60000);
+});
+
 </script>
 
 <style scoped>
